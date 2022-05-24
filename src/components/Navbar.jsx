6a2b6951bf1ee2cabeca223fc7e2d0b823fb7bdd
zubin-madon <madon.zubin@gmail.com>
2022-05-24 10:35:13 +0530

    icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,11 @@ const Navbar = () => {
         )
       })}
       
-      
+      <div onClick={()=>setOpen(!open)} className="hover:animate-pulse m-2 p-2 flex flex-wrap text-3xl cursor-pointer text-white">
+      <Link href="/">
+      <Image width="40" height="40" layout="intrinsic" className='rounded-lg w-10 h-10 object-contain object-top sm:mb-0 mb-4 hover:animate-pulse' src='/sofsafe-icon.png' alt='SofSafe logo'/>
+      </Link>
+      </div>
     </nav>
     </div>
   )
